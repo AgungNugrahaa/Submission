75e2208e474e25b326bf4ce9543bbfab5af9d70e
AgungNugrahaa <anugrah171819@gmail.com>
2025-01-22 15:14:29 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,7 @@ import streamlit as st
 import time
 
 # Membaca data
-day_df = pd.read_csv('/Users/agungnugraha/Documents/submission/data/day.csv')
+day_df = pd.read_csv('/Users/agungnugraha/Documents/day.csv')
 hour_df = pd.read_csv('/Users/agungnugraha/Documents/SQL/main/hour.csv')
 
 # Mengubah kolom tanggal menjadi datetime
